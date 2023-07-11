d9b9a830cd53bae60c3f03ad5208ff87fc274f51
Alessia Moison <adm2217@columbia.edu>
2023-07-11 17:47:22 +0200

    second solution of reversestring with for loop

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,7 +7,16 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  return str.split('').reverse().join('');
+  // return str.split('').reverse().join('');
+
+  // OPTION 2 WITH FOR LOOP
+  let reversed = '';
+
+	for (let char of str) {
+		reversed = char + reversed;
+	}
+
+	return reversed;
 };
 
 module.exports = reverse;
