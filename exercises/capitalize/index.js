6b04e9cb4710c4d955eae7ae83806a3137821331
Alessia Moison <adm2217@columbia.edu>
2023-07-17 19:46:13 +0200

    steps solution 1

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,30 +7,32 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {
-  let result = str[0].toUpperCase();
 
-  for (let i = 1; i < str.length; i++) {
-    if (str[i - 1] === " ") {
-      result += str[i].toUpperCase();
-    } else {
-      result += str[i];
-    };
+function capitalize(str) {
+  const words = [];
+
+  for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
   }
 
-  return result;
+  return words.join(' ');
 }
 
 // return capitalize('There');
 
 module.exports = capitalize;
 
-// function capitalize(str) {
-//   const words = [];
 
-//   for (let word of str.split(' ')) {
-//     words.push(word[0].toUpperCase() + word.slice(1));
+// function capitalize(str) {
+//   let result = str[0].toUpperCase();
+
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === " ") {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     };
 //   }
 
-//   return words.join(' ');
+//   return result;
 // }
