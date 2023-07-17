13231245a7c9b805576b29eb66428c0a4d339d89
Alessia Moison <adm2217@columbia.edu>
2023-07-17 17:25:24 +0200

    solution 2 of chunk using while loop and slice method

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,18 +9,33 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
+  // SOLUTION 2 USING WHILE LOOP AND SLICE METHOD
   const chunked = [];
+  let index = 0;
 
-  for (let ele of array) {
-    const last = chunked[chunked.length - 1]; // In JS chunked[-1] not supported
-    if (!last || last.length === size) {
-      chunked.push([ele]);
-    } else {
-       last.push(ele);
-    }
+  while (index < array.length ) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
   }
 
   return chunked;
 }
 
 module.exports = chunk;
+
+
+// function chunk(array, size) {
+//   const chunked = [];
+
+//   for (let ele of array) {
+//     const last = chunked[chunked.length - 1]; // In JS chunked[-1] not supported
+
+//     if (!last || last.length === size) {
+//       chunked.push([ele]);
+//     } else {
+//        last.push(ele);
+//     }
+//   }
+
+//   return chunked;
+// }
