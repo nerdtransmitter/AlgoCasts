ce855e285fcde92ab9208b72adaaece9300888af
Alessia Moison <adm2217@columbia.edu>
2023-07-18 13:24:38 +0200

    pyramid sol 2 with recursion

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,44 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// RECURSIVE SOLUTION
+function pyramid(n, row = 0, level = '') {
+  if (row === n) { // our base case
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const middleCol = Math.floor((2 * n - 1) / 2);
+  let add;
+
+  if (middleCol - row <= level.length && middleCol + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+
+  pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//   const middleCol = Math.floor((2 * n - 1) / 2);
+
+//   for (let row = 0; row < n ; row++) {
+//     let level = '';
+
+//     for (let col = 0; col < 2 * n - 1; col++) {
+//       if (middleCol - row <= col && middleCol + row >= col) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,38 +17,38 @@
 //       '### '
 //       '####'
 
-// function steps(n) {
-//   for (row = 0; row < n; row++) {
-//     let stair = '';
-//     for (col = 0; col < n; col++) {
-//       if (col <= row) {
-//         stair += '#';
-//       } else {
-//         stair += ' ';
-//       }
-//     }
-//     console.log(stair);
-//   }
-// }
+function steps(n) {
+  for (row = 0; row < n; row++) {
+    let stair = '';
+    for (col = 0; col < n; col++) {
+      if (col <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+    console.log(stair);
+  }
+}
 
 // RECURSIVE SOLUTION
-function steps(n, row = 0, stair = '') {
-  if (n === row) {
-    return;
-  }
+// function steps(n, row = 0, stair = '') {
+//   if (n === row) {
+//     return;
+//   }
 
-  if (n === stair.length) {
-    console.log(stair);
-    return steps(n, row + 1); // increment row after console logging a stair;
-  }
+//   if (n === stair.length) {
+//     console.log(stair);
+//     return steps(n, row + 1); // increment row after console logging a stair;
+//   }
 
-  if (stair.length <= row) {
-    stair += '#';
-  } else {
-    stair += ' ';
-  }
+//   if (stair.length <= row) {
+//     stair += '#';
+//   } else {
+//     stair += ' ';
+//   }
 
-  steps(n, row, stair);
-}
+//   steps(n, row, stair);
+// }
 
 module.exports = steps;
