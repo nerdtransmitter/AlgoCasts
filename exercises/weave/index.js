a6db4a1d9f4bc8bc096921fbb44bf30862d18acb
Alessia Moison <adm2217@columbia.edu>
2023-07-20 13:55:24 +0200

    final version of test with correct implementation of weave able of taking queues of different lengths

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -28,8 +28,12 @@ function weave(sourceOne, sourceTwo) {
   const q = new Queue;
 
   while (sourceOne.peek() || sourceTwo.peek()) {
-    q.add(sourceOne.remove());
-    q.add(sourceTwo.remove());
+    if (sourceOne.peek()) {
+      q.add(sourceOne.remove());
+    }
+    if (sourceTwo.peek()) {
+      q.add(sourceTwo.remove());
+    }
   }
 
   return q;
