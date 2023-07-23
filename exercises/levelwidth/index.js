d382c139f1144e23087ba56166ffbf3dba02bca0
Alessia Moison <adm2217@columbia.edu>
2023-07-23 21:12:40 +0200

    levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,21 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const arr = [root, 's']; // 's' = stopper value indicates end of value;
+  const counters = [0];
+
+  while (arr.length > 1) { // while array has something in it other than just 's'
+    const node = arr.shift();
+    if (node === 's') {
+      counters.push(0);
+      arr.push('s');
+    } else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+  return counters;
+}
 
 module.exports = levelWidth;
