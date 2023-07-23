017a7e64ca577c6dd129431ec0e918be0d46b172
Alessia Moison <adm2217@columbia.edu>
2023-07-23 19:30:10 +0200

    implemented tree node class

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,7 +10,22 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter(node => { // remember filter does not change array so reassign the filtered array to this.children
+      return node.data !== data;
+    });
+  }
+}
 
 class Tree {}
 
