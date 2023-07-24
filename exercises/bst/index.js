77731f635369ede4307915bff5f860640afa4d67
Alessia Moison <adm2217@columbia.edu>
2023-07-24 10:49:00 +0200

    BST contains solution

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -31,6 +31,32 @@ class Node {
       this.right = new Node(data);
     }
   }
+
+  contains(data) {
+    if (data === this.data) {
+      return this;
+    } else if (data > this.data && this.right) {
+      return this.right.contains(data);
+    } else if (data < this.data && this.left) {
+      return this.left.contains(data);
+    } else {
+      return null;
+    }
+    // if(data > this.data && this.right) {
+    //   if (data === this.right.data) {
+    //     return this.right;
+    //   } else {
+    //   this.right.contains(data);
+    //   }
+    // } else if (data < this.data && this.left) {
+    //   if (data === this.left.data) {
+    //     return this.left;
+    //   } else {
+    //     this.left.contains(data);
+    //   }
+    // }
+    // return null;
+  }
 }
 
 module.exports = Node;
