4b2ec50de2932804c877435ab58ac1072eefd2a2
Alessia Moison <adm2217@columbia.edu>
2023-07-27 19:28:45 +0200

    selectionSort implemented

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -17,7 +17,22 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
 
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+
+    if (i !== indexOfMin) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
 }
 
 function mergeSort(arr) {
